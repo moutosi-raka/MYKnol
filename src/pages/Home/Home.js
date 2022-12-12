dd144b687ccfe16f66d95469aeee9ace7b664618
moutosi-raka <raka29077997@gmail.com>
2022-12-12 10:20:29 +0600

    first commit

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import HomeNavbar from '../HomeNavbar/HomeNavbar';
+import road from '../../assests/road-672036_960_720.jpg'
+import '../Banner/Banner.css'
+
+const Home = () => {
+    return (
+        <div className='banner'>
+            <HomeNavbar></HomeNavbar>
+           
+        </div>
+    );
+};
+
+export default Home;
